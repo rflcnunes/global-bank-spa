7d7932a77efcf55acfd54f4844bf9b99396952e0
rafael <rafaelcnunes@icloud.com>
2023-05-22 01:07:50 -0300

    added not found

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: () => import('../views/NotFoundView.vue')
+    },
+    {
       path: '/',
       name: 'home',
       component: HomeView
@@ -14,14 +19,6 @@ const router = createRouter({
       name: 'features',
       component: () => import('../views/FeaturesView.vue')
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
   ]
 })
 
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col items-center mt-6 gap-6 p-4">
+    <div class="flex flex-row gap-2 items-center justify-center">
+      <span class="text-4xl font-bold">Not found... YET!</span>
+      <v-icon icon="mdi-account-hard-hat-outline" size="x-large" />
+    </div>
+
+    <p class="font-mono text-xl text-slate-600 max-w-md text-center mt-4">This page is still under construction, come back another time.</p>
+
+    <v-btn color="blue-accent-3" prepend-icon="mdi-arrow-left-thin" @click="$router.push({ name: 'home' })">
+      Back to Home
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFoundView'
+}
+</script>
